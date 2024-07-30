037403d1d8c59472b96dadb1e6d0a4c1555ba211
teruyuki-nakano0902 <teruyuki.319209@gmail.com>
2024-07-30 16:46:03 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 中野輝之 on 2024/07/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
